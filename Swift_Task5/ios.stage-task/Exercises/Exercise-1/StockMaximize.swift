26c80740b94b005521f0a556a5e526954089f60a
Anton Petrash <antonpetrash@Antons-MacBook-Air.local>
2021-06-14 09:23:14 +0300

    feat:add stockMaximize solution

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,15 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var profit = 0
+        var mutablePrices = prices
+        
+        for price in prices {
+            mutablePrices.removeFirst()
+            if let max = mutablePrices.max() , max > price  {
+                profit += max - price
+            }
+        }
+        return profit
     }
 }
